4f4bdee726fbd6b13db2f9182dd23d73009416f8
Raffaele Ragni <raffaele.ragni@gmail.com>
2012-06-16 17:09:31 +0200

    ImportOldTask is now separate from Activity

--- src/ki/wardrive4/activity/MapViewer.java
+++ src/ki/wardrive4/activity/MapViewer.java
@@ -19,13 +19,8 @@
 package ki.wardrive4.activity;
 
 import android.app.AlertDialog;
-import android.app.ProgressDialog;
-import android.content.ContentValues;
 import android.content.DialogInterface;
 import android.content.pm.ActivityInfo;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.Menu;
@@ -35,10 +30,7 @@ import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapView;
 import java.io.File;
 import ki.wardrive4.R;
-import ki.wardrive4.data.WiFiSecurity;
-import ki.wardrive4.provider.wifi.WiFiContract;
-import ki.wardrive4.utils.Geohash;
-import ki.wardrive4.utils.SHA1Utils;
+import ki.wardrive4.activity.tasks.ImportOldTask;
 
 /**
  * The main map viewer screen, a map showing WiFis currently in database.
@@ -91,9 +83,8 @@ public class MapViewer extends MapActivity
                                 case 0:
                                     File dbFile = new File(Environment.getExternalStorageDirectory(), "wardrive.db3");
                                     if (dbFile.exists() && dbFile.isFile())
-                                    {
-                                        new ImportTask().execute(dbFile);
-                                    }
+                                        // Start the background task
+                                        new ImportOldTask(MapViewer.this).execute(dbFile);
                                     else
                                         // Alert error for not finding a correct file
                                         new AlertDialog.Builder(MapViewer.this)
@@ -123,140 +114,5 @@ public class MapViewer extends MapActivity
     {
         // No route gets displayed in wardrive, only WiFi points.
         return false;
-    }
-    
-    /**
-     * Task for importing the old wardrive database.
-     */
-    private class ImportTask extends AsyncTask<File, Integer, Void>
-    {
-        private ProgressDialog progressDialog = null;
-        
-        @Override
-        protected Void doInBackground(File... paramss)
-        {
-            File databaseFile = paramss[0];
-            int ct = 1;
-            SQLiteDatabase db = SQLiteDatabase.openDatabase(databaseFile.getAbsolutePath(), null, SQLiteDatabase.OPEN_READONLY);
-            Cursor c = null;
-            try
-            {
-                c = db.query("networks", new String[]{"count(bssid)"}, null, null, null, null, null);
-                c.moveToNext();
-                int total = c.getInt(0);
-                c.close();
-                
-                c = db.query("networks", new String[]
-                {
-                    "bssid",
-                    "ssid",
-                    "capabilities",
-                    "level",
-                    "frequency",
-                    "lat",
-                    "lon",
-                    "alt",
-                    "timestamp"
-                }, null, null, null, null, null);
-                
-                while (c.moveToNext())
-                {
-                    String bssid = c.getString(c.getColumnIndex("bssid"));
-                    String id = bssid;
-                    try{ id = SHA1Utils.sha1(id); } catch (Exception e){}
-                    Cursor cCount = getContentResolver().query(WiFiContract.WiFi.uriById(id), new String[]{WiFiContract.WiFi._ID}, null, null, null);
-                    try
-                    {
-                        // Jump: record already exists
-                        if (cCount.moveToNext())
-                        {
-                            // Although count it in the total.
-                            publishProgress(ct++, total);
-                            continue;
-                        }
-                    }
-                    finally
-                    {
-                        cCount.close();
-                    }
-                    
-                    ContentValues cv = new ContentValues();
-
-                    double lat = c.getDouble(c.getColumnIndex("lat"));
-                    double lon = c.getDouble(c.getColumnIndex("lon"));
-                    double alt = c.getDouble(c.getColumnIndex("alt"));
-                    String geohash = new Geohash().encode(lat, lon);
-                    
-                    String capabilities = c.getString(c.getColumnIndex("capabilities"));
-                    WiFiSecurity security = WiFiSecurity.fromCapabilities(capabilities);
-
-                    cv.put(WiFiContract.WiFi._ID, id);
-                    cv.put(WiFiContract.WiFi.COLUMN_NAME_BSSID, c.getString(c.getColumnIndex("bssid")));
-                    cv.put(WiFiContract.WiFi.COLUMN_NAME_SSID, c.getString(c.getColumnIndex("ssid")));
-                    cv.put(WiFiContract.WiFi.COLUMN_NAME_CAPABILITIES, capabilities);
-                    cv.put(WiFiContract.WiFi.COLUMN_NAME_SECURITY, security.ordinal());
-                    cv.put(WiFiContract.WiFi.COLUMN_NAME_LEVEL, c.getInt(c.getColumnIndex("level")));
-                    cv.put(WiFiContract.WiFi.COLUMN_NAME_FREQUENCY, c.getInt(c.getColumnIndex("frequency")));
-
-                    cv.put(WiFiContract.WiFi.COLUMN_NAME_LAT, lat);
-                    cv.put(WiFiContract.WiFi.COLUMN_NAME_LON, lon);
-                    cv.put(WiFiContract.WiFi.COLUMN_NAME_ALT, alt);
-                    cv.put(WiFiContract.WiFi.COLUMN_NAME_GEOHASH, geohash);
-                    cv.put(WiFiContract.WiFi.COLUMN_NAME_TIMESTAMP, c.getLong(c.getColumnIndex("timestamp")));
-
-                    getContentResolver().insert(WiFiContract.WiFi.CONTENT_URI, cv);
-
-                    publishProgress(ct++, total);
-                    
-                    if (isCancelled())
-                        break;
-                }
-            }
-            finally
-            {
-                if (c != null)
-                    c.close();
-                db.close();
-            }
-            
-            return null;
-        }
-
-        @Override
-        protected void onProgressUpdate(Integer... values)
-        {
-            int ct = values[0];
-            int total = values[1];
-            
-            if (progressDialog == null)
-            {
-                progressDialog = new ProgressDialog(MapViewer.this);
-                progressDialog.setTitle(R.string.mapviewer_dlg_importoldprogress_title);
-                progressDialog.setMessage(getText(R.string.mapviewer_dlg_importoldprogress_message));
-                progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
-                // If canceled the user would not have any other way to see the progress, instead map the canceling to cancel the task
-//                progressDialog.setCancelable(false);
-                progressDialog.setOnCancelListener(new DialogInterface.OnCancelListener()
-                {
-                    @Override
-                    public void onCancel(DialogInterface di)
-                    {
-                        cancel(true);
-                    }
-                });
-                
-                progressDialog.show();
-            }
-            
-            progressDialog.setProgress(ct);
-            progressDialog.setMax(total);
-        }
-
-        @Override
-        protected void onPostExecute(Void result)
-        {
-            progressDialog.dismiss();
-            progressDialog = null;
-        }
     }
 }
---
+++ src/ki/wardrive4/activity/tasks/ImportOldTask.java
@@ -0,0 +1,182 @@
+/*
+ *   wardrive4 - android wardriving application (remake for the ICS)
+ *   Copyright (C) 2012 Raffaele Ragni
+ *   https://github.com/raffaeleragni/android-wardrive4
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *   
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package ki.wardrive4.activity.tasks;
+
+import android.app.ProgressDialog;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
+import java.io.File;
+import ki.wardrive4.R;
+import ki.wardrive4.data.WiFiSecurity;
+import ki.wardrive4.provider.wifi.WiFiContract;
+import ki.wardrive4.utils.Geohash;
+import ki.wardrive4.utils.SHA1Utils;
+
+/**
+ * Task for importing the old wardrive database.
+ * 
+ * @author Raffaele Ragni <raffaele.ragni@gmail.com>
+ */
+public class ImportOldTask extends AsyncTask<File, Integer, Void>
+{
+    private ProgressDialog progressDialog = null;
+    
+    private Context mContext;
+
+    public ImportOldTask(Context mContext)
+    {
+        this.mContext = mContext;
+    }
+
+    @Override
+    protected Void doInBackground(File... paramss)
+    {
+        File databaseFile = paramss[0];
+        int ct = 1;
+        SQLiteDatabase db = SQLiteDatabase.openDatabase(databaseFile.getAbsolutePath(), null, SQLiteDatabase.OPEN_READONLY);
+        Cursor c = null;
+        try
+        {
+            c = db.query("networks", new String[]{"count(bssid)"}, null, null, null, null, null);
+            c.moveToNext();
+            int total = c.getInt(0);
+            c.close();
+
+            c = db.query("networks", new String[]
+            {
+                "bssid",
+                "ssid",
+                "capabilities",
+                "level",
+                "frequency",
+                "lat",
+                "lon",
+                "alt",
+                "timestamp"
+            }, null, null, null, null, null);
+
+            while (c.moveToNext())
+            {
+                String bssid = c.getString(c.getColumnIndex("bssid"));
+                String id = bssid;
+                try
+                {
+                    id = SHA1Utils.sha1(id);
+                }
+                catch (Exception e)
+                {
+                }
+                Cursor cCount = mContext.getContentResolver().query(WiFiContract.WiFi.uriById(id), new String[]{WiFiContract.WiFi._ID }, null, null, null);
+                try
+                {
+                    // Jump: record already exists
+                    if (cCount.moveToNext())
+                    {
+                        // Although count it in the total.
+                        publishProgress(ct++, total);
+                        continue;
+                    }
+                }
+                finally
+                {
+                    cCount.close();
+                }
+
+                ContentValues cv = new ContentValues();
+
+                double lat = c.getDouble(c.getColumnIndex("lat"));
+                double lon = c.getDouble(c.getColumnIndex("lon"));
+                double alt = c.getDouble(c.getColumnIndex("alt"));
+                String geohash = new Geohash().encode(lat, lon);
+
+                String capabilities = c.getString(c.getColumnIndex("capabilities"));
+                WiFiSecurity security = WiFiSecurity.fromCapabilities(capabilities);
+
+                cv.put(WiFiContract.WiFi._ID, id);
+                cv.put(WiFiContract.WiFi.COLUMN_NAME_BSSID, c.getString(c.getColumnIndex("bssid")));
+                cv.put(WiFiContract.WiFi.COLUMN_NAME_SSID, c.getString(c.getColumnIndex("ssid")));
+                cv.put(WiFiContract.WiFi.COLUMN_NAME_CAPABILITIES, capabilities);
+                cv.put(WiFiContract.WiFi.COLUMN_NAME_SECURITY, security.ordinal());
+                cv.put(WiFiContract.WiFi.COLUMN_NAME_LEVEL, c.getInt(c.getColumnIndex("level")));
+                cv.put(WiFiContract.WiFi.COLUMN_NAME_FREQUENCY, c.getInt(c.getColumnIndex("frequency")));
+
+                cv.put(WiFiContract.WiFi.COLUMN_NAME_LAT, lat);
+                cv.put(WiFiContract.WiFi.COLUMN_NAME_LON, lon);
+                cv.put(WiFiContract.WiFi.COLUMN_NAME_ALT, alt);
+                cv.put(WiFiContract.WiFi.COLUMN_NAME_GEOHASH, geohash);
+                cv.put(WiFiContract.WiFi.COLUMN_NAME_TIMESTAMP, c.getLong(c.getColumnIndex("timestamp")));
+
+                mContext.getContentResolver().insert(WiFiContract.WiFi.CONTENT_URI, cv);
+
+                publishProgress(ct++, total);
+
+                if (isCancelled())
+                    break;
+            }
+        }
+        finally
+        {
+            if (c != null)
+                c.close();
+            db.close();
+        }
+
+        return null;
+    }
+
+    @Override
+    protected void onProgressUpdate(Integer... values)
+    {
+        int ct = values[0];
+        int total = values[1];
+
+        if (progressDialog == null)
+        {
+            progressDialog = new ProgressDialog(mContext);
+            progressDialog.setTitle(R.string.mapviewer_dlg_importoldprogress_title);
+            progressDialog.setMessage(mContext.getText(R.string.mapviewer_dlg_importoldprogress_message));
+            progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+            // If canceled the user would not have any other way to see the progress, instead map the canceling to cancel the task
+            progressDialog.setOnCancelListener(new DialogInterface.OnCancelListener()
+            {
+                @Override
+                public void onCancel(DialogInterface di)
+                {
+                    cancel(true);
+                }
+            });
+
+            progressDialog.show();
+        }
+
+        progressDialog.setProgress(ct);
+        progressDialog.setMax(total);
+    }
+
+    @Override
+    protected void onPostExecute(Void result)
+    {
+        progressDialog.dismiss();
+        progressDialog = null;
+    }
+}
